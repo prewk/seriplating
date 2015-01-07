22ca9f0bc4faa02afe1fd1f8eee575b6d7c54275
Oskar Thornblad <oskar.thornblad@gmail.com>
2015-01-07 14:54:44 +0100

    More unit tests for id resolver

--- tests/unit/Seriplating/IdResolverTest.php
+++ tests/unit/Seriplating/IdResolverTest.php
@@ -27,4 +27,32 @@ class IdResolverTest extends SeriplatingTestCase
         $this->assertEquals($foo0, 1);
         $this->assertEquals($foo1, 2);
     }
+
+    public function test_internal_id_arrays()
+    {
+        $resolver = new IdResolver;
+
+        $foo0 = null;
+        $shouldBeNull = "not null";
+        $foo1 = null;
+        $foo2 = null;
+
+        $resolver->bind("foos_0", 1);
+        $resolver->bind("foos_1", 2);
+        $resolver->bind("foos_2", 3);
+
+        $resolver->deferResolution(["foos_0", null, "foos_1", "foos_2"], function($dbId1, $isNull, $dbId2, $dbId3) use (&$foo0, &$shouldBeNull, &$foo1, &$foo2) {
+            $foo0 = $dbId1;
+            $shouldBeNull = $isNull;
+            $foo1 = $dbId2;
+            $foo2 = $dbId3;
+        });
+
+        $resolver->resolve();
+
+        $this->assertEquals(1, $foo0);
+        $this->assertEquals(null, $shouldBeNull);
+        $this->assertEquals(2, $foo1);
+        $this->assertEquals(3, $foo2);
+    }
 }
