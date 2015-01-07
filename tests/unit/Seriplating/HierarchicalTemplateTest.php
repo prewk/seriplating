cd56eb9ed20ae92028c326ec2f3e5dabdb5a2340
Oskar Thornblad <oskar.thornblad@gmail.com>
2015-01-07 15:59:24 +0100

    More unit tests for the hierarchical template

--- tests/unit/Seriplating/HierarchicalTemplateTest.php
+++ tests/unit/Seriplating/HierarchicalTemplateTest.php
@@ -15,7 +15,7 @@ class HierarchicalTemplateMock extends HierarchicalTemplate
 
 class HierarchicalTemplateTest extends SeriplatingTestCase
 {
-    public function test_registration()
+    public function test_that_registration_adds()
     {
         $hier = new HierarchicalTemplateMock;
         $t = new Seriplater(
@@ -34,5 +34,60 @@ class HierarchicalTemplateTest extends SeriplatingTestCase
         $hier->register($serializer);
 
         $this->assertEquals(["foos" => $serializer], $hier->getTemplateRegistry());
+    }
+
+    /**
+     * @expectedException \Prewk\Seriplating\Errors\HierarchicalCompositionException
+     */
+    public function test_that_missing_ids_fail()
+    {
+        $hier = new HierarchicalTemplate;
+        $t = new Seriplater(
+            new Rule
+        );
+
+        $template = [
+            "foo" => $t->value(),
+        ];
+
+        $serializer1 = Mockery::mock("Prewk\\Seriplating\\Contracts\\BidirectionalTemplateInterface");
+        $serializer1
+            ->shouldReceive("getTemplate")
+            ->andReturn($template);
+
+        $serializer2 = Mockery::mock("Prewk\\Seriplating\\Contracts\\BidirectionalTemplateInterface");
+        $serializer2
+            ->shouldReceive("getTemplate")
+            ->andReturn($template);
+
+        $hier->register($serializer1);
+    }
+
+    /**
+     * @expectedException \Prewk\Seriplating\Errors\HierarchicalCompositionException
+     */
+    public function test_that_duplicate_registrations_fail()
+    {
+        $hier = new HierarchicalTemplate;
+        $t = new Seriplater(
+            new Rule
+        );
+
+        $template = [
+            "id" => $t->id("foos"),
+        ];
+
+        $serializer1 = Mockery::mock("Prewk\\Seriplating\\Contracts\\BidirectionalTemplateInterface");
+        $serializer1
+            ->shouldReceive("getTemplate")
+            ->andReturn($template);
+
+        $serializer2 = Mockery::mock("Prewk\\Seriplating\\Contracts\\BidirectionalTemplateInterface");
+        $serializer2
+            ->shouldReceive("getTemplate")
+            ->andReturn($template);
+
+        $hier->register($serializer1);
+        $hier->register($serializer2);
     }
 }
