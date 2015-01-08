37ec33021b6615bc0cd6daca3a67a9b8cb91e931
Oskar Thornblad <oskar.thornblad@gmail.com>
2015-01-08 13:04:07 +0100

    Dox

--- src/Seriplating/GenericDeserializer.php
+++ src/Seriplating/GenericDeserializer.php
@@ -46,12 +46,22 @@ class GenericDeserializer implements DeserializerInterface
         $this->idResolver = $idResolver;
     }
 
-    public function deserialize(array $template, RepositoryInterface $repository, array $toUnserialize, array $inherited = [], $primaryKeyField = "id")
+    /**
+     * Deserialize according to the rules set up in the template into the repository
+     *
+     * @param array $template Seriplater template
+     * @param RepositoryInterface $repository Target repository
+     * @param array $toDeserialize Serialized data
+     * @param array $inherited Inherited data from a parent entity
+     * @param string $primaryKeyField Name of primary key field
+     * @return array The created entity
+     */
+    public function deserialize(array $template, RepositoryInterface $repository, array $toDeserialize, array $inherited = [], $primaryKeyField = "id")
     {
-        $this->toUnserialize = $toUnserialize;
+        $this->toUnserialize = $toDeserialize;
         $this->inherited = $inherited;
 
-        $entityData = $this->walkDeserializedData($template, $toUnserialize);
+        $entityData = $this->walkDeserializedData($template, $toDeserialize);
 
         $createdEntity = $repository->create($entityData);
         $primaryKey = $createdEntity[$primaryKeyField];
